f863bc44ad493dc2d4842dbdc8d9147dcc917e0c
Keval Shah <kevalshah1123@gmail.com>
2016-08-26 19:28:06 -0500

    Add basic things like number, shapes, and number generator

---
+++ Assets/Scripts/Smashee.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Smashee : MonoBehaviour {
+
+	public TextMesh number;
+
+	public int numToSmash = 5;
+	public int numRandomRange = 4; // one sided range on how off random number can be
+	public int numSafeRange = 1; // one sided range on 'safe zone' So if numToSmash = 5, then the number can't start from 4 to 6
+
+	int max;
+	int min;
+	int currentNum;
+
+	float timer = 0f;
+
+	void Start() {
+		
+		int max = numToSmash + numRandomRange;
+		int min = numToSmash - numRandomRange;
+
+		// set starting number
+		do currentNum = Random.Range(min, max);
+		while (currentNum >= numToSmash - numSafeRange && currentNum <= numToSmash + numSafeRange);
+
+		number.text = currentNum.ToString();
+	}
+	
+	void Update () {
+
+		timer += Time.deltaTime;
+
+		if (timer >= 1) {
+			UpdateNumber();
+			timer = 0;
+		}
+
+	}
+
+	void UpdateNumber() {
+
+		if (currentNum == numToSmash) LoseSmashee();
+
+		if (currentNum < numToSmash) currentNum++;
+		else currentNum--;
+
+		number.text = currentNum.ToString();
+	}
+
+	void LoseSmashee() {
+
+	}
+}
---
+++ Assets/Scripts/SmasheeGenerator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmasheeGenerator : MonoBehaviour {
+
+	public float instantiationSpeed;
+	public GameObject smashee;
+
+	float timer = 1f; // also acts as delay for when to start generating
+
+	void Start () {
+	}	
+	
+	void Update () {
+
+		timer -= Time.deltaTime;
+
+		if (timer <= 0) {
+
+			//TODO: Add grid-like snapping. The image is 256x256
+
+			float x = Random.value * Camera.main.pixelWidth;
+			float y = Random.value * Camera.main.pixelHeight;
+
+			Vector3 pos = new Vector3(x, y, 1);
+			pos = Camera.main.ScreenToWorldPoint(pos);
+
+			Instantiate(smashee, pos, Quaternion.identity);
+
+			timer = instantiationSpeed;
+		}
+		
+	}
+}
